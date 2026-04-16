e97055f20ee033235bdbd151531b46716469fa1c
Yahya Asım <yasimdoner@gmail.com>
2026-04-16 21:27:07 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="coming-soon-container">
+      <h1 className="giant-text">YAKINDA</h1>
+      <div className="glow"></div>
+    </div>
+  )
+}
+
+export default App
